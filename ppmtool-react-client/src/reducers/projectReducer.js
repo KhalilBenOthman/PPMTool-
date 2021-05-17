396de5ce6c3dc606f709ccffce3f4784bef6091a
KhalilBenOthman <khalil.b.othman@gmail.com>
2021-05-17 17:12:22 +0200

    CRUD Project ops

---
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -0,0 +1,32 @@
+import { GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "../actions/types";
+
+const initialStates = {
+  projects: [],
+  project: {},
+};
+
+export default function (state = initialStates, action) {
+  switch (action.type) {
+    case GET_PROJECTS:
+      return {
+        ...state,
+        projects: action.payload,
+      };
+    case GET_PROJECT:
+      return {
+        ...state,
+        project: action.payload,
+      };
+
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(
+          (project) => project.projectIdentifier !== action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+}
